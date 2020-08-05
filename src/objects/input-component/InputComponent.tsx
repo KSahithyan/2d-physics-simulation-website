02589a7f21fd9654f0d2b9f881bc11bc71f58b9a
KSahithyan <sahithyan2701@gmail.com>
2020-08-05 12:21:14 +0530

    input-container added

---
+++ src/objects/input-component/InputComponent.tsx
@@ -0,0 +1,25 @@
+import React, { ChangeEvent } from 'react';
+const css = require('./input-component.css');
+
+interface PropTypes {
+    type?: string,
+    placeholder?: string,
+    id?: string,
+    label?: string,
+    value: string,
+    onChangeListener: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+export const InputComponent = (props: PropTypes) => {
+    let id = props.id || props.label;
+    return (
+        <div className="input-container">
+            {props.label ? <label htmlFor={id}>{props.label}</label> : null}
+            <input
+                type={props.type || 'text'}
+                id={id} placeholder={props.placeholder || ''}
+                value={props.value}
+                onChange={e => props.onChangeListener(e)} ></input>
+        </div>
+    )
+}
